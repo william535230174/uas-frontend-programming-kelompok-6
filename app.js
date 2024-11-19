1ab1a9e334d9e09980d8e2c534a41f68eb6ab8de
MichaelDominic <michaelchristiandominic@gmail.com>
2024-11-20 03:38:21 +0700

    membuat halaman products

---
+++ app.js
@@ -0,0 +1,78 @@
+angular.module("shoeStoreApp", []).controller("ProductController", function () {
+  this.products = [
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+    {
+      name: "Mechanical Keyboard",
+      description: "Menggunakan Blue Swicth.",
+      price: 100.99,
+      stars: [1, 1, 1, 1, 1],
+      image: "images/keyboard1.jpg",
+    },
+  ];
+
+  this.addToCart = function (product) {
+    alert(product.name + " masuk keranjang!");
+  };
+});

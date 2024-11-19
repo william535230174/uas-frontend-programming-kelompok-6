7af3c394c5541fe7986129a44388695aa3a3dc53
MichaelDominic <michaelchristiandominic@gmail.com>
2024-11-20 04:01:16 +0700

    membuat halaman products

--- app.js
+++ app.js
@@ -1,78 +1,80 @@
-angular.module("shoeStoreApp", []).controller("ProductController", function () {
-  this.products = [
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-    {
-      name: "Mechanical Keyboard",
-      description: "Menggunakan Blue Swicth.",
-      price: 100.99,
-      stars: [1, 1, 1, 1, 1],
-      image: "images/keyboard1.jpg",
-    },
-  ];
+angular
+  .module("electronicApp", [])
+  .controller("ProductController", function () {
+    this.products = [
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+      {
+        name: "Mechanical Keyboard",
+        description: "Menggunakan Blue Swicth.",
+        price: 100.99,
+        stars: [1, 1, 1, 1, 1],
+        image: "images/keyboard1.jpg",
+      },
+    ];
 
-  this.addToCart = function (product) {
-    alert(product.name + " masuk keranjang!");
-  };
-});
+    this.addToCart = function (product) {
+      alert(product.name + " masuk keranjang!");
+    };
+  });
